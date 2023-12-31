9621a7c49be0c7650ffff60a986c592783c51434
Casey Li <sometimescasey@gmail.com>
2023-12-31 00:12:18 -0500

    work

---
+++ ruby_basics/4_debugging.rb
@@ -0,0 +1,12 @@
+def isogram?(string)
+  original_length = string.length
+  string_array = string.downcase.split
+  if string_array.length != 1
+    puts 'Please provide exactly one word.'
+    return nil
+  end
+  unique_length = string.downcase.chars.uniq.length
+  original_length == unique_length
+end
+
+puts isogram?('hhey')
