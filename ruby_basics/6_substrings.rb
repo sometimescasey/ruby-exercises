d816293eec2317dc0eca9f7a17711ba840209976
Casey Li <sometimescasey@gmail.com>
2024-01-09 16:04:43 -0500

    different logic depending on whether overlapping of substrings is allowed

--- ruby_basics/6_substrings.rb
+++ ruby_basics/6_substrings.rb
@@ -1,4 +1,4 @@
-def substrings(string, dictionary)
+def substrings_overlap(string, dictionary)
   string_as_array = string.downcase.chars
   # for each n-char long word from the dict, scan along string_as_array n-chars at a time
   dictionary.each_with_object(Hash.new(0)) do |word, result|
@@ -10,7 +10,20 @@ def substrings(string, dictionary)
   end
 end
 
+def substrings_no_overlap(string, dictionary)
+  dictionary.each_with_object(Hash.new(0)) do |word, result|
+    result[word] += string.downcase.scan(word).length unless string.downcase.scan(word).empty?
+  end
+end
+
+def substrings(string, dictionary, allow_overlap: true)
+  allow_overlap ? substrings_overlap(string, dictionary) : substrings_no_overlap(string, dictionary)
+end
+
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-p substrings('below', dictionary)
-p '-' * 5
-p substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+dictionary = %w[oo ooo]
+puts substrings('oooo', dictionary) # {"oo"=>3, "ooo"=>2}
+puts substrings('oooo', dictionary, allow_overlap: false) # {"oo"=>2, "ooo"=>1}
